2368d1c8380622d39287b6ca903a980c1b294c60
Paul Stone <stone.paul.green@gmail.com>
2025-12-31 02:33:55 +0300

    Add a singly linked list data type

---
+++ include/SinglyList.h
@@ -0,0 +1,132 @@
+#ifndef _SINGLY_LINKED_LISTS_H
+#define _SINGLY_LINKED_LISTS_H
+
+#include <stdlib.h>
+
+typedef struct node {
+
+    void* data;
+    struct node* next;
+    void* sentinel;
+};
+
+typedef struct node Node;
+
+typedef struct {
+
+    int size;
+
+    Node* head;
+    Node* tail;
+
+    void (*destroy)(void* data);
+} sList;
+
+/* ================================================================ */
+
+/**
+ * Initializes the linked list specified by `list`. This operation must
+ * be called for a linked list before the list can be used with any other operation.
+ * The `destroy` argument provides a way to free dynamically allocated data when
+ * `ist_destroy` is called. For example, if the list contains data dynamically allocated
+ * using `malloc`, destroy should be set to `free` to free the data as the linked list is
+ * destroyed. For structured data containing several dynamically allocated members,
+ * `destroy` should be set to a user-defined function that calls free for each dynamically
+ * allocated member as well as for the structure itself. For a linked list containing
+ * data that should not be freed, destroy should be set to `NULL`.
+ * 
+ * @return None.
+ */
+void sList_init(sList* list, void (*destroy)(void* data));
+
+/**
+ * Destroys the linked list specified by `list`. No other operations
+ * are permitted after calling `sList_destroy` unless `sList_init` is called again
+ * The `sList_destroy` operation removes all elements from a linked list and calls the function
+ * passed as `destroy` to `sList_init` once for each element as it is removed, provided
+ * destroy was not set to `NULL`.
+ * 
+ * @return None.
+ */
+void sList_destroy(sList* list);
+
+/**
+ * Inserts an element at the tail of the list specified by `list`.
+ * The new element contains a pointer to `data`, so the memory referenced by `data`
+ * should remain valid as long as the element remains in the list. It is the responsibility
+ * of the caller to manage the storage associated with `data`.
+ * 
+ * @return `0` if inserting the element is successful, or `–1` otherwise.
+ */
+int sList_insert_last(sList* list, const void* data);
+
+/**
+ * Inserts an element at the head of the list specified by `list`.
+ * The new element contains a pointer to `data`, so the memory referenced by `data`
+ * should remain valid as long as the element remains in the list. It is the responsibility
+ * of the caller to manage the storage associated with `data`.
+ * 
+ * @return `0` if inserting the element is successful, or `–1` otherwise.
+ */
+int sList_insert_first(sList* list, const void* data);
+
+/**
+ * Removes the element at the tail of the list specified by `list`.
+ * Returns a pointer to the data that was stored in the removed element,
+ * or `NULL` if the list is empty. The caller takes ownership of the returned
+ * data pointer and is responsible for freeing its associated memory when no longer needed.
+ * 
+ * @return A pointer to the data, or `NULL` if the list is empty.
+ */
+void* sList_remove_last(sList* list);
+
+/**
+ * Removes the element at the head of the list specified by `list`.
+ * Returns a pointer to the data that was stored in the removed element,
+ * or `NULL` if the list is empty. The caller takes ownership of the returned
+ * data pointer and is responsible for freeing its associated memory when no longer needed.
+ * 
+ * @return A pointer to the data, or `NULL` if the list is empty.
+ */
+void* sList_remove_first(sList* list);
+
+/**
+ * Searches for the first occurrence of data in the list specified by `list`
+ * using the user-defined `match` function. The match function should return non-zero
+ * for a successful match between `key1` (node data) and `key2` (search data).
+ * 
+ * @return A pointer to the node containing the matching data, or `NULL` if no match is found.
+ */
+Node* sList_find(const sList* list, const void* data, int (*match)(const void* key1, const void* key2));
+
+/**
+ * Removes the specified node from the `list`.
+ * The node must be a valid node previously obtained from the list (e.g., via `sList_find_node`).
+ * The caller takes ownership of the returned data pointer and is responsible
+ * for freeing its associated memory when no longer needed.
+ * 
+ * @return A pointer to the data that was stored in the removed node, or NULL if the node is invalid.
+ */
+void* sList_remove(sList* list, Node* node);
+
+/**
+ * Inserts a new node containing data just after the specified node in the linked list specified by `list`.
+ * The new node contains a pointer to `data`, so the memory referenced by `data` should remain valid as
+ * long as the node remains in the list. 
+ * It is the responsibility of the caller to manage the storage associated with data.
+ * 
+ * @return `0` if successful, or `-1` if the operation fails.
+ */
+int sList_insert_after(sList* list, Node* node, const void* data);
+
+/**
+ * Inserts a new node containing data just before the specified node in the list specified by `list`.
+ * The new node contains a pointer to `data`, so the memory referenced by `data` should remain valid as
+ * long as the node remains in the list.
+ * It is the responsibility of the caller to manage the storage associated with data.
+ * 
+ * @return `0` if successful, or `-1` if the operation fails.
+ */
+int sList_insert_before(sList* list, Node* node, const void* data);
+
+#endif /* _SINGLY_LINKED_LISTS_H */
---
+++ source/SinglyList.c
@@ -0,0 +1,278 @@
+#include "../include/SinglyList.h"
+
+#include <stdio.h>
+
+/* ================================================================ */
+/* ============================ STATIC ============================ */
+/* ================================================================ */
+
+static Node* _create_node(const void* data) {
+
+    Node* node = NULL;
+    /* ======== */
+
+    if ((node = calloc(1, sizeof(Node))) == NULL) {
+        return NULL;
+    }
+
+    node->data = (void*) data;
+
+    /* ======== */
+    return node;
+}
+
+/* ================================================================ */
+/* ========================== INTERFACE =========================== */
+/* ================================================================ */
+
+void sList_init(sList* list, void (*destroy)(void* data)) {
+
+    list->size = 0;
+    list->head = list->tail = NULL;
+    list->destroy = destroy;
+}
+
+/* ================================================================ */
+
+void sList_destroy(sList* list) {
+
+    void* data = NULL;
+    /* ======== */
+
+    while (list->size > 0) {
+
+        data = sList_remove_first(list);
+
+        if (list->destroy != NULL) { list->destroy(data); }
+    }
+}
+
+/* ================================================================ */
+
+int sList_insert_last(sList* list, const void* data) {
+
+    Node* node = NULL;
+    /* ======== */
+
+    if ((node = _create_node(data)) == NULL) {
+        return -1;
+    }
+
+    if (list->size == 0) {
+        list->head = list->tail = node;
+    }
+    else {
+        list->tail->next = node;
+        list->tail = node;
+    }
+
+    node->sentinel = list;
+    list->size++;
+
+    /* ======== */
+    return 0;
+}
+
+/* ================================================================ */
+
+int sList_insert_first(sList* list, const void* data) {
+
+    Node* node = NULL;
+    /* ======== */
+
+    if ((node = _create_node((void*) data)) == NULL) {
+        return -1;
+    }
+
+    if (list->size == 0) {
+        list->head = list->tail = node;
+    }
+    else {
+        node->next = list->head;
+        list->head = node;
+    }
+
+    node->sentinel = list;
+    list->size++;
+
+    /* ======== */
+    return 0;
+}
+
+/* ================================================================ */
+
+void* sList_remove_last(sList* list) {
+
+    Node* previous = NULL;
+    Node* current = NULL;
+    void* data = NULL;
+    /* ======== */
+
+    if (list->size > 0) {
+
+        data = list->tail->data;
+        current = list->tail;
+
+        if (list->size == 1) {
+            list->head = list->tail = NULL;
+        }
+        else {
+            for (previous = list->head, current = previous->next; current != list->tail; previous = current, current = current->next) ;
+
+            list->tail = previous;
+            list->tail->next = NULL;
+        }
+
+        free(current);
+        list->size--;
+    }
+
+    /* ======== */
+    return data;
+}
+
+/* ================================================================ */
+
+void* sList_remove_first(sList* list) {
+
+    Node* node = NULL;
+    void* data = NULL;
+    /* ======== */
+
+    /* === Do not allow removal from an empty list === */
+    if (list->size > 0) {
+
+        data = list->head->data;
+        node = list->head;
+
+        if (list->size == 1) {
+            list->head = list->tail = NULL;
+        }
+        else {
+            list->head = list->head->next;
+        }
+
+        free(node);
+        list->size--;
+    }
+
+    /* ======== */
+    return data;
+}
+
+/* ================================================================ */
+
+Node* sList_find(const sList* list, const void* data, int (*match)(const void* key1, const void* key2)) {
+
+    if ((list->size == 0) || (match == NULL) || (data == NULL)) {
+        return NULL;
+    }
+
+    for (Node* t = list->head; t != NULL; t = t->next) {
+        
+        if (match(data, t->data) == 1) {
+            return t;
+        }
+    }
+
+    /* ======== */
+    return NULL;
+}
+
+/* ================================================================ */
+
+void* sList_remove(sList* list, Node* node) {
+
+    Node* previous = NULL;
+    Node* current = NULL;
+
+    void* data = NULL;
+    /* ======== */
+
+    if (list->size == 0) {
+        return NULL;
+    }
+
+    if (node->sentinel != list) {
+        return NULL;
+    }
+
+    if (node == list->head) { return sList_remove_first(list); }
+
+    if (node == list->tail) { return sList_remove_last(list); }
+
+    for (previous = list->head, current = previous->next; current != node; previous = current, current = current->next) ;
+
+    previous->next = current->next;
+    current->next = NULL;
+
+    free(node);
+    list->size--;
+
+    /* ======== */
+    return 0;
+}
+
+/* ================================================================ */
+
+int sList_insert_after(sList* list, Node* node, const void* data) {
+
+    Node* t = NULL;
+    Node* n = NULL;
+
+    /* ======== */
+
+    if ((list->size == 0) || (node == NULL) || (data == NULL) || (node->sentinel != list)) {
+        return -1;
+    }
+
+    if (node == list->tail) { return sList_insert_last(list, data); }
+
+    if ((n = _create_node(data)) == NULL) {
+        return -1;
+    }
+
+    for (t = list->head; t != node; t = t->next) ;
+
+    n->sentinel = list;
+    n->next = t->next;
+    node->next = n;
+
+    list->size++;
+
+    /* ======== */
+    return 0;
+}
+
+/* ================================================================ */
+
+int sList_insert_before(sList* list, Node* node, const void* data) {
+
+    Node* t = NULL;
+    Node* n = NULL;
+
+    /* ======== */
+
+    if ((list->size == 0) || (node == NULL) || (data == NULL) || (node->sentinel != list)) {
+        return -1;
+    }
+
+    if (node == list->head) { return sList_insert_first(list, data); }
+
+    if ((n = _create_node(data)) == NULL) {
+        return -1;
+    }
+
+    for (t = list->head; t->next != node; t = t->next) ;
+
+    n->sentinel = list;
+    n->next = node;
+    t->next = n;
+
+    list->size++;
+
+    /* ======== */
+    return 0;
+}
+
+/* ================================================================ */
