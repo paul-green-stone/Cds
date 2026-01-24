2158e4cc2ba6b15c67b44a138c5abf7540e5803a
Paul Stone <stone.paul.green@gmail.com>
2026-01-25 01:28:51 +0300

    Update a queue data structure

--- include/Queue.h
+++ include/Queue.h
@@ -4,78 +4,74 @@
 #include "SinglyList.h"
 
 typedef sList Queue;
-typedef sListResult QueueResult;
 
 /**
- * Initializes the queue specified by `queue`. This operation must be
- * called for a queue before the queue can be used with any other operation.
- * The `destroy` argument provides a way to free dynamically allocated data when
- * `Queue_destroy` is called. For a queue containing data that should not be freed,
- * `destroy` should be set to `NULL`.
+ * Initializes the queue specified by `queue`. This operation must
+ * be called for a queue before the queue can be used with any other
+ * operation. The `destroy` argument provides a way to free
+ * dynamically allocated data when `Queue_destroy` is called.
+ * For a queue containing data that should not be freed, `destroy`
+ * should be set to `NULL`.
  * 
- * @param queue The queue to initialize.
- * @param destroy The function used to free the data in each queue element when the queue is destroyed.
- * 
- * @note On failure, `last_error` is set to indicate the reason.
+ * @param queue     Pointer to the queue to initialize.
+ * @param destroy   Optional destructor called on each element during destroy.
  *  
- * @return `SLIST_OK` on success, error code otherwise.
+ * @return `CONTAINER_SUCCESS` on success, error code otherwise.
  */
-QueueResult Queue_init(Queue *queue, void (*destroy)(void *data));
+int Queue_init(Queue *queue, void (*destroy)(void *data));
 
 /**
  * Destroys the queue specified by `queue`. No other operations are
- * permitted after calling `Queue_destroy` unless `Queue_init` is called again.
- * The `Queue_destroy` operation removes all elements from a queue and calls the function passed
- * as `destroy` to `Queue_init` once for each element as it is removed, provided
- * destroy was not set to `NULL`.
+ * permitted after calling `Queue_destroy` unless `Queue_init` is
+ * called again. The `Queue_destroy` operation removes all elements
+ * from a queue and calls the function passed as `destroy` to
+ * `Queue_init` once for each element as it is removed, provided
+ * `destroy` was not set to `NULL`.
  * 
- * @param queue The queue to destroy.
+ * @param queue Pointer to the queue to destroy.
  * 
- * @note On failure, `last_error` is set to indicate the reason.
- * 
- * @return `SLIST_OK` on success, error code otherwise.
+ * @return `CONTAINER_SUCCESS` on success, error code otherwise.
  */
-QueueResult Queue_destroy(Queue* queue);
+int Queue_destroy(Queue* queue);
 
 /**
  * Enqueues an element at the tail of the queue specified by `queue`.
- * The new element contains a pointer to `data`, so the memory referenced by `data`
- * should remain valid as long as the element remains in the queue. It is the
- * responsibility of the caller to manage the storage associated with `data`.
+ * The new element contains a pointer to `data`, so the memory
+ * referenced by `data` should remain valid as long as the element
+ * remains in the queue. 
  * 
- * @param queue The queue to enqueue the element onto.
- * @param data A pointer to the data to store in the queue.
+ * It is the responsibility of the caller to manage the storage
+ * associated with `data`.
  * 
- * @note On failure, `last_error` is set to indicate the reason.
+ * @param queue     The queue to enqueue the element onto.
+ * @param data      A pointer to the data to store in the queue.
  * 
- * @return `SLIST_OK` on success, error code otherwise.
+ * @return `CONTAINER_SUCCESS` on success, error code otherwise.
  */
-QueueResult Queue_enqueue(Queue* queue, const void* data);
+int Queue_enqueue(Queue* queue, void* data);
 
 /**
  * Dequeues an element from the head of the queue specified by `queue`.
+ * 
  * It is the responsibility of the caller to manage the storage associated
  * with the data.
  * 
  * @param queue The queue to pop the element from.
  * 
- * @note On failure, `last_error` is set to indicate the reason.
- * 
- * @return A pointer to the data that was stored in the queue, or `NULL` if the queue is empty.
+ * @return A pointer to the data that was stored in the queue, `NULL` otherwise.
  */
 void* Queue_dequeue(Queue* queue);
 
 /**
  * This operation does not remove the first element from the queue.
- * The returned pointer remains valid as long as the element remains in the queue,
- * and the caller should not modify or free the data through this pointer.
+ * The returned pointer remains valid as long as the element remains
+ * in the queue, and the caller should not modify or free the data
+ * through this pointer.
  * 
  * @param queue The queue to peek at.
  * 
- * @note On failure, `last_error` is set to indicate the reason.
- * 
- * @return A pointer to the data stored in the front element of the queue, or `NULL` if the queue is empty.
+ * @return A pointer to the data stored in the front element of the queue, `NULL` otherswise.
  */
-const void* Queue_peek(Queue* queue);
+const void* Queue_peek(const Queue* queue);
 
 #endif /* QUEUE_H */
--- source/Queue.c
+++ source/Queue.c
@@ -1,33 +1,34 @@
 #include "../include/Queue.h"
 
-QueueResult Queue_init(Queue *queue, void (*destroy)(void *data)) {
+int Queue_init(Queue *queue, void (*destroy)(void *data)) {
     return sList_init(queue, destroy, NULL);
 }
 
-QueueResult Queue_destroy(Queue* queue) {
+int Queue_destroy(Queue* queue) {
     return sList_destroy(queue);
 }
 
-QueueResult Queue_enqueue(Queue* queue, const void* data) {
+int Queue_enqueue(Queue* queue, void* data) {
     return sList_insert_last(queue, data);
 }
 
 void* Queue_dequeue(Queue* queue) {
-    return sList_remove_first(queue);
-}
 
-const void* Queue_peek(Queue* queue) {
-
-    if (queue == NULL) {
-        return NULL;
-    }
-    
-    if (queue->size > 0) {
-        queue->last_error = SLIST_ERR_EMPTY;
-        /* ======== */
-        return sNode_data(queue->head);
-    }
+    void* data = NULL;
+    /* ======== */
+    sList_remove_first(queue, &data);
 
     /* ======== */
-    return NULL;
+    return data;
+}
+
+const void* Queue_peek(const Queue* queue) {
+
+    const void* data = NULL;
+    /* ======== */
+
+    data = sNode_data(sList_head(queue));
+
+    /* ======== */
+    return data;
 }
